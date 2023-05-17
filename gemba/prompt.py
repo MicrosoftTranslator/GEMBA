0fdcd8d820f0dabd7b333c6c3a46cadb22758db3
Mayank Soni <mayank.soni101@gmail.com>
2023-05-17 17:44:31 +0800

    Removed black formatter edits

--- gemba/prompt.py
+++ gemba/prompt.py
@@ -14,7 +14,7 @@ def parse_and_check_numerical_answer(answer, min=None, max=None):
 
 def parse_numerical_answer(answer, min=None, max=None):
     # get all numbers in a string
-    numbers = re.findall(r"\d+", answer)
+    numbers = re.findall(r'\d+', answer)
     if len(numbers) == 1:
         return int(numbers[0])
 
@@ -98,55 +98,42 @@ prompts = {
     "GEMBA-DA": {
         "prompt": 'Score the following translation from {source_lang} to {target_lang} on a continuous scale from 0 to 100, where a score of zero means "no meaning preserved" and score of one hundred means "perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} translation: "{target_seg}"\nScore: ',
         "validate_answer": lambda x: validate_number(x),
-        "use_ref": False,
-    },
+        "use_ref": False},
+
     "GEMBA-DA_ref": {
         "prompt": 'Score the following translation from {source_lang} to {target_lang} with respect to human reference on a continuous scale 0 to 100 where score of zero means "no meaning preserved" and score of one hundred means "perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} human reference: "{reference_seg}"\n{target_lang} machine translation: "{target_seg}"\nScore: ',
         "validate_answer": lambda x: validate_number(x),
-        "use_ref": True,
-    },
+        "use_ref": True},
+
     "GEMBA-SQM": {
         "prompt": 'Score the following translation from {source_lang} to {target_lang} on a continuous scale from 0 to 100 that starts on "No meaning preserved", goes through "Some meaning preserved", then "Most meaning preserved and few grammar mistakes", up to "Perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} translation: "{target_seg}"\nScore (0-100): ',
         "validate_answer": lambda x: validate_number(x),
-        "use_ref": False,
-    },
+        "use_ref": False},
+
     "GEMBA-SQM_ref": {
         "prompt": 'Score the following machine translation from {source_lang} to {target_lang} with respect to the human reference on a continuous scale from 0 to 100 that starts with "No meaning preserved", goes through "Some meaning preserved", then "Most meaning preserved and few grammar mistakes", up to "Perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} human reference: "{reference_seg}"\n{target_lang} machine translation: "{target_seg}"\nScore (0-100): ',
         "validate_answer": lambda x: validate_number(x),
-        "use_ref": True,
-    },
+        "use_ref": True},
+
     "GEMBA-stars": {
         "prompt": 'Score the following translation from {source_lang} to {target_lang} with one to five stars. Where one star means "Nonsense/No meaning preserved", two stars mean "Some meaning preserved, but not understandable", three stars mean "Some meaning preserved and understandable", four stars mean "Most meaning preserved with possibly few grammar mistakes", and five stars mean "Perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} translation: "{target_seg}"\nStars: ',
         "validate_answer": lambda x: validate_stars(x),
-        "use_ref": False,
-    },
+        "use_ref": False},
+
     "GEMBA-stars_ref": {
         "prompt": 'Score the following translation from {source_lang} to {target_lang} with respect to the human reference with one to five stars. Where one star means "Nonsense/No meaning preserved", two stars mean "Some meaning preserved, but not understandable", three stars mean "Some meaning preserved and understandable", four stars mean "Most meaning preserved with possibly few grammar mistakes", and five stars mean "Perfect meaning and grammar".\n\n{source_lang} source: "{source_seg}"\n{target_lang} human reference: "{reference_seg}"\n{target_lang} translation: "{target_seg}"\nStars: ',
         "validate_answer": lambda x: validate_stars(x),
-        "use_ref": True,
-    },
+        "use_ref": True},
+
     "GEMBA-classes": {
         "prompt": 'Classify the quality of machine translation from {source_lang} to {target_lang} into one of following classes: "No meaning preserved", "Some meaning preserved, but not understandable", "Some meaning preserved and understandable", "Most meaning preserved, minor issues", "Perfect translation".\n\n{source_lang} source: "{source_seg}"\n{target_lang} machine translation: "{target_seg}"\nClass: ',
         "use_ref": False,
-        "validate_answer": lambda x, classes=[
-            "No meaning preserved",
-            "Some meaning preserved, but not understandable",
-            "Some meaning preserved and understandable",
-            "Most meaning preserved, minor issues",
-            "Perfect translation",
-        ]: parse_classes(x, classes),
-        "max_tokens": 100,
-    },
+        "validate_answer": lambda x, classes=["No meaning preserved", "Some meaning preserved, but not understandable", "Some meaning preserved and understandable", "Most meaning preserved, minor issues", "Perfect translation"]: parse_classes(x, classes),
+        "max_tokens": 100},
+
     "GEMBA-classes_ref": {
         "prompt": 'Classify the quality of machine translation from {source_lang} to {target_lang} with respect to the human reference into one of following classes: "No meaning preserved", "Some meaning preserved, but not understandable", "Some meaning preserved and understandable", "Most meaning preserved, minor issues", "Perfect translation".\n\n{source_lang} source: "{source_seg}"\n{target_lang} human reference: "{reference_seg}"\n{target_lang} machine translation: "{target_seg}"\nClass: ',
         "use_ref": True,
-        "validate_answer": lambda x, classes=[
-            "No meaning preserved",
-            "Some meaning preserved, but not understandable",
-            "Some meaning preserved and understandable",
-            "Most meaning preserved, minor issues",
-            "Perfect translation",
-        ]: parse_classes(x, classes),
-        "max_tokens": 100,
-    },
+        "validate_answer": lambda x, classes=["No meaning preserved", "Some meaning preserved, but not understandable", "Some meaning preserved and understandable", "Most meaning preserved, minor issues", "Perfect translation"]: parse_classes(x, classes),
+        "max_tokens": 100},
 }
