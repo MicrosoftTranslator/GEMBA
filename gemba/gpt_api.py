ee4028ba19a43f8a110413d2838808029726f06b
Tom Kocmi <14163064+kocmitom@users.noreply.github.com>
2023-10-26 01:02:24 +0200

    updated GPT calls

--- gemba/gpt_api.py
+++ gemba/gpt_api.py
@@ -1,5 +1,7 @@
 import openai
+import sys
 import time
+import ipdb
 from termcolor import colored
 from datetime import datetime
 
@@ -16,7 +18,7 @@ class GptApi:
         if "api_base" in credentials:
             # Azure API access
             openai.api_type = "azure"
-            openai.api_version = "2022-06-01-preview"
+            openai.api_version = "2023-05-15"
             openai.api_base = credentials["api_base"]
             openai.api_key = credentials["api_key"]
             self.api_type = "azure"
@@ -32,9 +34,10 @@ class GptApi:
             self.rps_limit = 0
         self.last_call_timestamp = 0
 
+        self.non_batchable_models = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k"]
+
     # answer_id is used for determining if it was the top answer or how deep in the list it was
-    def request(self, prompt, model, parse_response, temperature=0, answer_id=-1, cache=None):
-        max_tokens = 20
+    def request(self, prompt, model, parse_response, temperature=0, answer_id=-1, cache=None, max_tokens=20):
         answers = None
         if cache is not None:
             answers = cache.get({
@@ -71,7 +74,7 @@ class GptApi:
             answer_id += 1
             answer = parse_response(full_answer)
             if self.verbose or temperature > 0:
-                print(f"Answer (t={temperature}): " + colored(answer, "yellow") + " (" + colored(full_answer, "blue") + ")")
+                print(f"Answer (t={temperature}): " + colored(answer, "yellow") + " (" + colored(full_answer, "blue") + ")", file=sys.stderr)
             if answer is None:
                 continue
             parsed_answers.append(
@@ -109,8 +112,6 @@ class GptApi:
 
         self.last_call_timestamp = ts
 
-        if self.verbose:
-            print(prompt)
         while True:
             try:
                 response = self.call_api(prompt, model, n, temperature, max_tokens)
@@ -120,19 +121,22 @@ class GptApi:
                 if hasattr(e, 'code'):
                     if e.code == 'content_filter':
                         return []
-                    print(e.code)
+                    print(e.code, file=sys.stderr)
                 # frequent error is reaching the API limit
-                print(colored("Error, retrying...", "red"))
-                print(e)
+                print(colored("Error, retrying...", "red"), file=sys.stderr)
+                print(e, file=sys.stderr)
                 time.sleep(1)
 
         answers = []
         for choice in response["choices"]:
-            answer = choice['text'].strip()
+            if "message" in choice:
+                answer = choice['message']['content'].strip()
+            else:
+                answer = choice['text'].strip()
             # one of the responses didn't finish, we need to request more tokens
-            if choice["finish_reason"] != "stop" and model != "text-chat-davinci-002":  # TODO remove exception
+            if choice["finish_reason"] != "stop":
                 if self.verbose:
-                    print(colored(f"Increasing max tokens to fit answers.", "red") + colored(answer, "blue"))
+                    print(colored(f"Increasing max tokens to fit answers.", "red") + colored(answer, "blue"), file=sys.stderr)
                 return self.request_api(prompt, model, temperature=temperature, max_tokens=max_tokens + 200)
 
             answers.append({
@@ -146,26 +150,51 @@ class GptApi:
 
         return answers
 
-    def call_api(self, prompt, model, n, temperature, max_tokens):
+    def call_api(self, prompt, model, n, temperature, max_tokens):        
+        parameters = {
+            "temperature": temperature/10,
+            "max_tokens": max_tokens,
+            "top_p": 1,
+            "n": n,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": None,
+            "request_timeout": 30
+        }
+
         if self.api_type == "azure":
-            return openai.Completion.create(
-                engine=self.deployments[model],
-                prompt=prompt,
-                temperature=temperature/10,
-                max_tokens=max_tokens,
-                top_p=1,
-                n=n,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=None)
+            parameters["engine"] = self.deployments[model]
         else:
-            return openai.Completion.create(
-                model=self.deployments[model],
-                prompt=prompt,
-                temperature=temperature/10,
-                max_tokens=max_tokens,
-                top_p=1,
-                n=n,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=None)
+            parameters["model"] = self.deployments[model]
+
+        if model in self.non_batchable_models:
+            if isinstance(prompt, list):
+                # check that prompt contain list of dictionaries with role and content
+                assert all(isinstance(p, dict) for p in prompt), "Prompts must be a list of dictionaries."
+                assert all("role" in p and "content" in p for p in prompt), "Prompts must be a list of dictionaries with role and content."
+
+
+                parameters["messages"] = prompt
+            else:
+                parameters["messages"] = [{
+                    "role": "user",
+                    "content": prompt,
+                }]
+
+            completion_function = openai.ChatCompletion.create
+        else:
+            # check that prompt is a list of strings
+            assert isinstance(prompt, str), "prompt must be a strings."
+
+            parameters["prompt"] = prompt
+            completion_function = openai.Completion.create
+
+        return completion_function(**parameters)
+    
+    def bulk_request(self, df, model, parse_mqm_answer, cache, max_tokens=20):
+        answers = []
+        for i, row in df.iterrows():
+            prompt = row["prompt"]
+            parsed_answers = self.request(prompt, model, parse_mqm_answer, cache=cache, max_tokens=max_tokens)
+            answers += parsed_answers
+        return answers
